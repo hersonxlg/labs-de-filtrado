873563c023158790d0422d9d31b0fdffd21deb3e
herson <hersonx@gmail.com>
2022-07-24 16:47:33 -0600

    se crea otra version para maximizar ventana tanto en windows como en linux

--- lab01/graficar2.py
+++ lab01/graficar2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os, sys
+import platform as pf
 
 # detectar ruta donde esta el ejecutable.
 application_path = os.path.dirname(sys.executable)
@@ -15,7 +16,10 @@ plt.xticks(range(0,365,36))
 plt.grid()
 # Abrir la ventana de la grafica maximizada.
 fig_manager = plt.get_current_fig_manager()
-fig_manager.window.maximize()
+if pf.system() == 'Windows':
+    fig_manager.window.showMaximized()
+else:
+    fig_manager.window.maximize()
 plt.xlabel("Fecha", color="red", fontsize="17")
 plt.ylabel("Precio en $", color="red", fontsize="17")
 titulo = "Precio diario del Bitcoin durante el ultimo año."
