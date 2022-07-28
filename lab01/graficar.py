e0e8e813784968a92c23d48c7bc6cabc4caa7535
herson <hersonx@gmail.com>
2022-07-28 15:08:31 -0600

    incrementar el tamaño de los lables de cada eje del figure

--- lab01/graficar.py
+++ lab01/graficar.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import os, sys
-
-# detectar ruta donde esta el ejecutable.
-application_path = os.path.dirname(sys.executable)
+import numpy as np
 
 df = pd.read_csv('market-price.csv')
 
@@ -11,13 +8,11 @@ x = df.Timestamp.apply(lambda datetime: datetime.split()[0])
 y = df['market-price']
 
 plt.plot(x,y)
-plt.xticks(range(0,365,36))
+plt.xticks(np.linspace(0,365,8,dtype=int))
+plt.tick_params(labelsize=13)
 plt.grid()
-# Abrir la ventana de la grafica maximizada.
-fig_manager = plt.get_current_fig_manager()
-fig_manager.window.maximize()
-plt.xlabel("Fecha", color="red", fontsize="17")
-plt.ylabel("Precio en $", color="red", fontsize="17")
+plt.xlabel("Fecha", color="green", fontsize="17", fontweight="bold")
+plt.ylabel("Precio en $", color="green", fontsize="17", fontweight="bold")
 titulo = "Precio diario del Bitcoin durante el ultimo año."
-plt.title(titulo, color="red", fontsize="24")
+plt.title(titulo, color="red", fontsize="24", fontweight="bold")
 plt.show()
--- lab01/graficar2.py
+++ lab01/graficar2.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import os, sys
+import numpy as np
 import platform as pf
-
-# detectar ruta donde esta el ejecutable.
-application_path = os.path.dirname(sys.executable)
 
 df = pd.read_csv('market-price.csv')
 
@@ -12,7 +9,8 @@ x = df.Timestamp.apply(lambda datetime: datetime.split()[0])
 y = df['market-price']
 
 plt.plot(x,y)
-plt.xticks(range(0,365,36))
+plt.xticks(np.linspace(0,365,8,dtype=int))
+plt.tick_params(labelsize=13)
 plt.grid()
 # Abrir la ventana de la grafica maximizada.
 fig_manager = plt.get_current_fig_manager()
@@ -20,8 +18,8 @@ if pf.system() == 'Windows':
     fig_manager.window.showMaximized()
 else:
     fig_manager.window.maximize()
-plt.xlabel("Fecha", color="red", fontsize="17")
-plt.ylabel("Precio en $", color="red", fontsize="17")
+plt.xlabel("Fecha", color="green", fontsize="17", fontweight="bold")
+plt.ylabel("Precio en $", color="green", fontsize="17", fontweight="bold")
 titulo = "Precio diario del Bitcoin durante el ultimo año."
-plt.title(titulo, color="red", fontsize="24")
+plt.title(titulo, color="red", fontsize="24", fontweight="bold")
 plt.show()
